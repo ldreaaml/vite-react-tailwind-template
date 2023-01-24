b20f80abc0b98405faef5123485949ceb46ebfb6
Dream Jitsawatpaiboon <dreamza43@gmail.com>
2023-01-24 21:31:29 +1300

    template

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-8xl underline text-red">Hello World</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        white: "#ffffff",
+      },
+    },
+  },
+  plugins: [],
+};
